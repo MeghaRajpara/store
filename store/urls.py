622c632709f57ecb3a5576b89a6dbc7c2e9320fa
Megha Rajpara <rajpara.megha@gmail.com>
2023-07-07 13:08:25 -0400

    Modify cart model and create add to cart api

--- store/urls.py
+++ store/urls.py
@@ -6,13 +6,18 @@ from . import views
 router = routers.DefaultRouter()
 router.register('products', views.ProductViewSet, basename='products')
 router.register('collections', views.CollectionViewSet)
+router.register('carts', views.CartViewSet)
 
 products_router = routers.NestedDefaultRouter(router, 'products', lookup='product')
 products_router.register('reviews', views.ReviewViewSet, basename='product-reviews')
+
+carts_router = routers.NestedDefaultRouter(router, 'carts', lookup='cart')
+carts_router.register('items', views.CartItemsViewSet, basename='cart-items')
 # URL Conf
 # urlpatterns = router.urls + products_router.urls
 urlpatterns = [
     path(r'', include(router.urls)),
     path(r'', include(products_router.urls)),
+    path(r'', include(carts_router.urls)),
 ]
 
